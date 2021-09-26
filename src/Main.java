4dab94dda09c0b1dee73b33b4e065b9e24cb28d3
Maciejx7 <maciej.raulin@gmail.com>
2021-09-26 08:54:44 +0200

    Wstęp

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        /* Przykłady wyświetlania tekstu w konsoli */
-        System.out.println("Witaj świecie!");
-        System.out.println("Test kodu na ĆWICZENIE 1");
-
-        System.out.printf("No %s jest %d przykład.\n", "to", 1);
-
-        System.out.print("To jest jakiś");
-        System.out.print(" tekst");
-        System.out.print(" nie za długi.");
-//      test komentarza Raulin
+        System.out.println("Witam Raulin!");
     }
 }
