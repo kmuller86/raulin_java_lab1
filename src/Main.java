60fabfa5852540a75a5f54e9d1a2da39b83d0407
Maciejx7 <maciej.raulin@gmail.com>
2021-09-26 10:07:43 +0200

    Ćwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,23 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        System.out.println("Ćwiczenie 4");
+        System.out.println("Arytmetyczne:");
+        boolean a = true, b = true, c = false;
+        System.out.println("(a && b) : " + (a && b)); //t
+        System.out.println("(a && c) : " + (a && c)); //f
+        System.out.println("!(a && b) : " + !(a && b)); //f
+        System.out.println("!a : " + !a); //f
+        System.out.println("!b : " + !b); // f
+        System.out.println("!c : " + !c); // t
+        System.out.println("!((b && c) || (!c || a) : " + !((b && c) || (!c || a))); // f
+        System.out.println("Logiczne:");
+        int d=3, e=7, f=10;
+        System.out.println("(d == f) : " + (d==f)); //f
+        System.out.println("(f != e) : " + (f != e)); //t
+        System.out.println("(e > f) : " + (e > f)); //f
+        System.out.println("(e < f) : " + (e < f)); //t
+        System.out.println("(d >= f) : " + (d >= f)); //f
 
     }
 }
