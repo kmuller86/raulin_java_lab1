cc1500501cf7cc6494d0069c6a761cd01532648f
Maciejx7 <maciej.raulin@gmail.com>
2021-09-26 08:52:25 +0200

    ćwiczenie 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-//        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
+        /* Przykłady wyświetlania tekstu w konsoli */
+        System.out.println("Witaj świecie!");
+        System.out.println("Test kodu na ĆWICZENIE 1");
 
+        System.out.printf("No %s jest %d przykład.\n", "to", 1);
 
+        System.out.print("To jest jakiś");
+        System.out.print(" tekst");
+        System.out.print(" nie za długi.");
+//      test komentarza Raulin
     }
 }
