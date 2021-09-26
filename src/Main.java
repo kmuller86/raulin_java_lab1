153936a3b94a2926087dc03d0f599e0889a59693
Maciejx7 <maciej.raulin@gmail.com>
2021-09-26 11:19:27 +0200

    Ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,14 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
-
+        System.out.println("Ćwiczenie 6");
+        double[] tabela = new double[5];
+        tabela[0] = 2.5;
+        tabela[1] = 4.1;
+        tabela[2] = 8.7;
+        tabela[3] = 16.3;
+        tabela[4] = 32.1;
+        System.out.println("Zawartośc tablicy: "+ tabela);
+        }
     }
-}
+
